9ee1e3e1dc4a37583d3e9d7ae16c5102a58c04ec
Robyn Tomson <robyntomson@gmail.com>
2025-03-16 20:52:27 +0200

    Fixed some bugs and errors that were stopping the application from starting up

--- src/main/java/com/Anyro0/flight/model/Seat.java
+++ src/main/java/com/Anyro0/flight/model/Seat.java
@@ -1,5 +1,6 @@
 package com.Anyro0.flight.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,7 +22,9 @@ public class Seat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "seat_row")
     private int row;
+    
     private int seatNumber;
     private boolean isOccupied;
     private boolean isWindowSeat;
